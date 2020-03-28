5b5ea81bb6d866c5bfb4aad500a06b4591b6fe62
Alfredo Padilla <alfredo3133@gmail.com>
2020-03-28 18:33:35 +0000

    Finished Duck.

---
+++ src/_06_duck/Duck.java
@@ -0,0 +1,22 @@
+package _06_duck;
+
+public class Duck {
+	
+	private static final String String = null;
+
+		String favoriteFood;
+		int numberOfFriends;
+		Duck(String favoriteFood, int numberOfFriends) {
+			this.favoriteFood = favoriteFood;
+	      	this.numberOfFriends = numberOfFriends;
+		}
+		public void quack() {
+			System.out.println("Quack!");
+			
+		}
+		public void waddle() {
+			System.out.println("Waddle");
+			
+		}
+		
+}
