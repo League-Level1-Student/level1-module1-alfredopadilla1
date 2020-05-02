b6de7898cea246c7816098f99559a186b73f7e40
Alfredo Padilla <alfredo3133@gmail.com>
2020-05-02 18:39:01 +0000

    Working on tea party.

--- src/TeaPartyTest.java
+++ src/TeaPartyTest.java
@@ -2,12 +2,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TeaPartyTest {
- @Test
- public void test() {
-     TeaParty teaParty = new TeaParty();
-      String greeting = teaParty.welcome(null, false, false, false);
-      assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false, false));
-      assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false, false));
-      assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true, false));
- }
+	@Test
+	public void test() {
+		TeaParty teaParty = new TeaParty();
+		String greeting = teaParty.welcome(null, false, false, false);
+		assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false, false));
+		assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false, false));
+		assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true, false));
+	}
 }
