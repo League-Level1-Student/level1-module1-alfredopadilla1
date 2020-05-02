98c55b0399583e7825d0b4daaf371b40ba77a22d
Alfredo Padilla <alfredo3133@gmail.com>
2020-05-02 18:25:29 +0000

    Working on TeaParty.

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,19 +1,6 @@
 
 public class TeaParty {
-        public String welcome(String name, boolean isWoman, boolean isKnighted) {
-			return name;
-//			have to write if statements to make sure that each are either woman/male/lady/sir/ms/mr
-			//Jane Austen is a woman, so say “Hello Ms. Austen”. 
-        	
-        	
-        }
+	public String welcome(String name, boolean isWoman, boolean isKnighted, boolean male) {
+		return name;
+	}
 }
-
-
-//* George Orwell is a man, so say “Hello Mr. Orwell”. 
-//* Isaac Newton was knighted, so say "Hello Sir Isaac Newton".
-//
-//
-//   
-        
-  
